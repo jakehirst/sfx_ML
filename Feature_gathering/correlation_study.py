bcd6c87ae985dd2b8eddeb62021694e39e60e095
jakehirst <jakehirst1022@gmail.com>
2023-05-20 11:06:47 -0600

    added correlation study and regression metric vis

--- Feature_gathering/correlation_study.py
+++ Feature_gathering/correlation_study.py
@@ -74,33 +74,122 @@ impact_sites_list = full_df['impact_sites'].to_numpy()
 # Transform each list element into a numpy array
 impact_sites = np.array([np.array(ast.literal_eval(string)) for string in impact_sites_list])
 
-impact_sites_x, impact_sites_y, impact_sites_z = convert_coordinates_to_new_basis(Material_X, Material_Y, Material_Z, CM, impact_sites[:,0], impact_sites[:,1], impact_sites[:,2])
-init_x, init_y, init_z = convert_coordinates_to_new_basis(Material_X, Material_Y, Material_Z, CM, full_df['init x'].to_numpy(), full_df['init y'].to_numpy(), full_df['init z'].to_numpy())
-front_0_x, front_0_y, front_0_z = convert_coordinates_to_new_basis(Material_X, Material_Y, Material_Z, CM, full_df['front 0 x'].to_numpy(), full_df['front 0 y'].to_numpy(), full_df['front 0 z'].to_numpy())
-front_1_x, front_1_y, front_1_z = convert_coordinates_to_new_basis(Material_X, Material_Y, Material_Z, CM, full_df['front 1 x'].to_numpy(), full_df['front 1 y'].to_numpy(), full_df['front 1 z'].to_numpy())
+full_df = insert_column_into_df(full_df, 'impact_sites', 'impact site x', impact_sites[:,0])
+full_df = insert_column_into_df(full_df, 'impact_sites', 'impact site y', impact_sites[:,1])
+full_df = insert_column_into_df(full_df, 'impact_sites', 'impact site z', impact_sites[:,2])
+labels = ['height', 'phi', 'theta', 'impact site x', 'impact site y', 'impact site z']
 
-impact_sites_r, impact_sites_phi, impact_sites_theta = convert_cartesian_to_spherical(impact_sites_x, impact_sites_y, impact_sites_z)
-init_r, init_phi, init_theta = convert_cartesian_to_spherical(init_x, init_y, init_z)
-front0_r, front0_phi, front0_theta = convert_cartesian_to_spherical(front_0_x, front_0_y, front_0_z)
-front1_r, front1_phi, front1_theta = convert_cartesian_to_spherical(front_1_x, front_1_y, front_1_z)
-
-full_df = insert_column_into_df(full_df, 'init z', 'init theta', init_theta)
-full_df = insert_column_into_df(full_df, 'init z', 'init phi', init_phi)
-full_df = insert_column_into_df(full_df, 'init z', 'init r', init_r)
-
-full_df = insert_column_into_df(full_df, 'front 0 z', 'front 0 theta', front0_theta)
-full_df = insert_column_into_df(full_df, 'front 0 z', 'front 0 phi', front0_phi)
-full_df = insert_column_into_df(full_df, 'front 0 z', 'front 0 r', front0_r)
-
-full_df = insert_column_into_df(full_df, 'front 1 z', 'front 1 theta', front1_theta)
-full_df = insert_column_into_df(full_df, 'front 1 z', 'front 1 phi', front1_phi)
-full_df = insert_column_into_df(full_df, 'front 1 z', 'front 1 r', front1_r)
-
-full_df = insert_column_into_df(full_df, 'impact_sites', 'impact site theta', impact_sites_theta)
-full_df = insert_column_into_df(full_df, 'impact_sites', 'impact site phi', impact_sites_phi)
-full_df = insert_column_into_df(full_df, 'impact_sites', 'impact site r', impact_sites_r)
-
+""" correlation study before everything is converted to Jimmy's reference frame """
+corr_matrix, p_matrix, important_features = Pearson_correlation(full_df, 'height', minimum_p_value=0.05)
+important_features  = [x for x in important_features if x not in labels]
+print(corr_matrix['height'][important_features])
 corr_matrix, p_matrix, important_features = Pearson_correlation(full_df, 'phi', minimum_p_value=0.05)
+important_features  = [x for x in important_features if x not in labels]
+print(corr_matrix['phi'][important_features])
+corr_matrix, p_matrix, important_features = Pearson_correlation(full_df, 'theta', minimum_p_value=0.05)
+important_features  = [x for x in important_features if x not in labels]
+print(corr_matrix['theta'][important_features])
+corr_matrix, p_matrix, important_features = Pearson_correlation(full_df, 'impact site x', minimum_p_value=0.05)
+important_features  = [x for x in important_features if x not in labels]
+print(corr_matrix['impact site x'][important_features])
+corr_matrix, p_matrix, important_features = Pearson_correlation(full_df, 'impact site y', minimum_p_value=0.05)
+important_features  = [x for x in important_features if x not in labels]
+print(corr_matrix['impact site y'][important_features])
+corr_matrix, p_matrix, important_features = Pearson_correlation(full_df, 'impact site z', minimum_p_value=0.05)
+important_features  = [x for x in important_features if x not in labels]
+print(corr_matrix['impact site z'][important_features])
+
+
+
+
+Jimmy_impact_sites_x, Jimmy_impact_sites_y, Jimmy_impact_sites_z = convert_coordinates_to_new_basis(Material_X, Material_Y, Material_Z, CM, impact_sites[:,0], impact_sites[:,1], impact_sites[:,2])
+Jimmy_init_x, Jimmy_init_y, Jimmy_init_z = convert_coordinates_to_new_basis(Material_X, Material_Y, Material_Z, CM, full_df['init x'].to_numpy(), full_df['init y'].to_numpy(), full_df['init z'].to_numpy())
+Jimmy_front_0_x, Jimmy_front_0_y, Jimmy_front_0_z = convert_coordinates_to_new_basis(Material_X, Material_Y, Material_Z, CM, full_df['front 0 x'].to_numpy(), full_df['front 0 y'].to_numpy(), full_df['front 0 z'].to_numpy())
+Jimmy_front_1_x, Jimmy_front_1_y, Jimmy_front_1_z = convert_coordinates_to_new_basis(Material_X, Material_Y, Material_Z, CM, full_df['front 1 x'].to_numpy(), full_df['front 1 y'].to_numpy(), full_df['front 1 z'].to_numpy())
+
+Jimmy_impact_sites_r, Jimmy_impact_sites_phi, Jimmy_impact_sites_theta = convert_cartesian_to_spherical(Jimmy_impact_sites_x, Jimmy_impact_sites_y, Jimmy_impact_sites_z)
+Jimmy_init_r, Jimmy_init_phi, Jimmy_init_theta = convert_cartesian_to_spherical(Jimmy_init_x, Jimmy_init_y, Jimmy_init_z)
+Jimmy_front0_r, Jimmy_front0_phi, Jimmy_front0_theta = convert_cartesian_to_spherical(Jimmy_front_0_x, Jimmy_front_0_y, Jimmy_front_0_z)
+Jimmy_front1_r, Jimmy_front1_phi, Jimmy_front1_theta = convert_cartesian_to_spherical(Jimmy_front_1_x, Jimmy_front_1_y, Jimmy_front_1_z)
+
+full_df = insert_column_into_df(full_df, 'init z', 'Jimmy_init theta', Jimmy_init_theta)
+full_df = insert_column_into_df(full_df, 'init z', 'Jimmy_init phi', Jimmy_init_phi)
+full_df = insert_column_into_df(full_df, 'init z', 'Jimmy_init r', Jimmy_init_r)
+
+full_df = insert_column_into_df(full_df, 'front 0 z', 'Jimmy_front 0 theta', Jimmy_front0_theta)
+full_df = insert_column_into_df(full_df, 'front 0 z', 'Jimmy_front 0 phi', Jimmy_front0_phi)
+full_df = insert_column_into_df(full_df, 'front 0 z', 'Jimmy_front 0 r', Jimmy_front0_r)
+
+full_df = insert_column_into_df(full_df, 'front 1 z', 'Jimmy_front 1 theta', Jimmy_front1_theta)
+full_df = insert_column_into_df(full_df, 'front 1 z', 'Jimmy_front 1 phi', Jimmy_front1_phi)
+full_df = insert_column_into_df(full_df, 'front 1 z', 'Jimmy_front 1 r', Jimmy_front1_r)
+
+full_df = insert_column_into_df(full_df, 'impact_sites', 'Jimmy_impact site theta', Jimmy_impact_sites_theta)
+full_df = insert_column_into_df(full_df, 'impact_sites', 'Jimmy_impact site phi', Jimmy_impact_sites_phi)
+full_df = insert_column_into_df(full_df, 'impact_sites', 'Jimmy_impact site r', Jimmy_impact_sites_r)
+
+full_df = insert_column_into_df(full_df, 'impact_sites', 'Jimmy_impact site z', Jimmy_impact_sites_z)
+full_df = insert_column_into_df(full_df, 'impact_sites', 'Jimmy_impact site y', Jimmy_impact_sites_y)
+full_df = insert_column_into_df(full_df, 'impact_sites', 'Jimmy_impact site x', Jimmy_impact_sites_x)
+
+full_df = insert_column_into_df(full_df, 'init z', 'Jimmy_init z', Jimmy_init_z)
+full_df = insert_column_into_df(full_df, 'init z', 'Jimmy_init y', Jimmy_init_y)
+full_df = insert_column_into_df(full_df, 'init z', 'Jimmy_init x', Jimmy_init_x)
+
+full_df = insert_column_into_df(full_df, 'front 0 z', 'Jimmy_front_0_z', Jimmy_front_0_z)
+full_df = insert_column_into_df(full_df, 'front 0 z', 'Jimmy_front_0_y', Jimmy_front_0_y)
+full_df = insert_column_into_df(full_df, 'front 0 z', 'Jimmy_front_0_x', Jimmy_front_0_x)
+
+full_df = insert_column_into_df(full_df, 'front 1 z', 'Jimmy_front_1_z', Jimmy_front_1_z)
+full_df = insert_column_into_df(full_df, 'front 1 z', 'Jimmy_front_1_y', Jimmy_front_1_y)
+full_df = insert_column_into_df(full_df, 'front 1 z', 'Jimmy_front_1_x', Jimmy_front_1_x)
+
+labels = ['height', 'phi', 'theta', 'impact site x', 'impact site y', 'impact site z', 'Jimmy_impact site x', 'Jimmy_impact site y', 
+          'Jimmy_impact site z', 'Jimmy_impact site r', 'Jimmy_impact site phi', 'Jimmy_impact site theta']
+
+""" correlation study after everything is converted to Jimmy's reference frame """
+corr_matrix, p_matrix, important_features = Pearson_correlation(full_df, 'height', minimum_p_value=0.05)
+important_features  = [x for x in important_features if x not in labels]
+print(corr_matrix['height'][important_features])
+corr_matrix, p_matrix, important_features = Pearson_correlation(full_df, 'phi', minimum_p_value=0.05)
+important_features  = [x for x in important_features if x not in labels]
+print(corr_matrix['phi'][important_features])
+corr_matrix, p_matrix, important_features = Pearson_correlation(full_df, 'theta', minimum_p_value=0.05)
+important_features  = [x for x in important_features if x not in labels]
+print(corr_matrix['theta'][important_features])
+
+corr_matrix, p_matrix, important_features = Pearson_correlation(full_df, 'impact site x', minimum_p_value=0.05)
+important_features  = [x for x in important_features if x not in labels]
+print(corr_matrix['impact site x'][important_features])
+corr_matrix, p_matrix, important_features = Pearson_correlation(full_df, 'impact site y', minimum_p_value=0.05)
+important_features  = [x for x in important_features if x not in labels]
+print(corr_matrix['impact site y'][important_features])
+corr_matrix, p_matrix, important_features = Pearson_correlation(full_df, 'impact site z', minimum_p_value=0.05)
+important_features  = [x for x in important_features if x not in labels]
+print(corr_matrix['impact site z'][important_features])
+
+corr_matrix, p_matrix, important_features = Pearson_correlation(full_df, 'Jimmy_impact site x', minimum_p_value=0.05)
+important_features  = [x for x in important_features if x not in labels]
+print(corr_matrix['Jimmy_impact site x'][important_features])
+corr_matrix, p_matrix, important_features = Pearson_correlation(full_df, 'Jimmy_impact site y', minimum_p_value=0.05)
+important_features  = [x for x in important_features if x not in labels]
+print(corr_matrix['Jimmy_impact site y'][important_features])
+corr_matrix, p_matrix, important_features = Pearson_correlation(full_df, 'Jimmy_impact site z', minimum_p_value=0.05)
+important_features  = [x for x in important_features if x not in labels]
+print(corr_matrix['Jimmy_impact site z'][important_features])
+
+corr_matrix, p_matrix, important_features = Pearson_correlation(full_df, 'Jimmy_impact site r', minimum_p_value=0.05)
+important_features  = [x for x in important_features if x not in labels]
+print(corr_matrix['Jimmy_impact site r'][important_features])
+corr_matrix, p_matrix, important_features = Pearson_correlation(full_df, 'Jimmy_impact site phi', minimum_p_value=0.05)
+important_features  = [x for x in important_features if x not in labels]
+print(corr_matrix['Jimmy_impact site phi'][important_features])
+corr_matrix, p_matrix, important_features = Pearson_correlation(full_df, 'Jimmy_impact site theta', minimum_p_value=0.05)
+important_features  = [x for x in important_features if x not in labels]
+print(corr_matrix['Jimmy_impact site theta'][important_features])
+
+full_df.to_csv("/Users/jakehirst/Desktop/sfx/sfx_ML_code/sfx_ML/Feature_gathering/FULL_OG_dataframe_with_impact_sites_and_Jimmy_RF.csv")
+
 
 
 # impact_r, impact_phi, impact_theta = 
