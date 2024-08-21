75f31834243ba7085f0c395fa30fe031bd64a540
jakehirst <jakehirst1022@gmail.com>
2024-08-21 09:18:37 -0600

    finished commenting ANN_regression

--- Feature_gathering/latin_hypercube_sampling.py
+++ Feature_gathering/latin_hypercube_sampling.py
@@ -2,19 +2,19 @@
 from pyDOE import lhs
 import numpy as np
 
-# Number of samples
+#Number of samples
 num_samples = 1000
 
-# Variable ranges
+#Variable ranges
 height_range = (1.0, 4.0)
 phi_range = (5, 50)
 theta_range = (0, 360)
 
-# Generate Latin Hypercube samples
+#Generate Latin Hypercube samples
 variable_ranges = [height_range, phi_range, theta_range]
 lhs_samples = lhs(len(variable_ranges), samples=num_samples)
 
-# Scale and convert LHS samples to variable types
+#Scale and convert LHS samples to variable types
 scaled_samples = []
 for i, (lower, upper) in enumerate(variable_ranges):
     if i == 0:
@@ -23,9 +23,9 @@ for i, (lower, upper) in enumerate(variable_ranges):
         scaled_samples.append(lhs_samples[:, i] * (upper - lower) + lower)
         scaled_samples[i] = scaled_samples[i].astype(int)
 
-# Transpose the scaled samples
+#Transpose the scaled samples
 lhs_samples_transposed = list(map(list, zip(*scaled_samples)))
 
-# Print the LHS samples
+#Print the LHS samples
 for sample in lhs_samples_transposed:
     print(str(sample).replace("[","").replace("]",""))
